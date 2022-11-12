fa9431e1a2ee3f7a1c4a3d22413ed734a54529c0
Barreto <vargasbarreto@hotmail.com>
2022-11-12 08:41:33 -0300

    Adicionando projeto lab Dio

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens........"
+
+docker build -t barretovargas/projeto-backend:1.0 backend/.
+docker build -t barretovargas/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens...."
+
+docker push barretovargas/projeto-backend:1.0
+docker push barretovargas/projeto-database:1.0
+
+echo "Criando serviços no cluster Kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments......"
+
+kubectl apply -f ./deployment.yml
